90c393d3abad46774978fb60f4f03d0fd158d35f
Weblate <noreply@weblate.org>
2022-06-09 19:59:50 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (29 of 29 strings)
    
    Co-authored-by: Weblate Admin <blacksheep@nubecula.org>
    Translate-URL: http://weblate.nubecula.org/projects/harbour-zollstock/harbour-zollstock/de/
    Translation: harbour-zollstock/harbour-zollstock

--- translations/harbour-zollstock-de.ts
+++ translations/harbour-zollstock-de.ts
@@ -1,126 +1,126 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de">
 <context>
     <name></name>
     <message id="id-app-name">
         <source>Zollstock</source>
-        <translation type="unfinished"></translation>
+        <translation>Zollstock</translation>
     </message>
     <message id="id-about">
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Über</translation>
     </message>
     <message id="id-about-page-description">
         <source>You need to measure a length, but you don&apos;t have a ruler with you? Then use this app to get the job done.</source>
         <oldsource>Du musst eine Länge messen, hast aber kein Lineal dabei? Then use this app to get the job done.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Du musst eine Länge messen, hast aber kein Lineal dabei? Dann verwende diese App, um die Arbeit zu erledigen.</translation>
     </message>
     <message id="id-translations">
         <source>Translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Übersetzungen</translation>
     </message>
     <message id="id-about-page-translation-text">
         <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Deine Sprache ist nicht verfügbar? Du kannst das Projekt gerne unterstützen, indem Du eine Übersetzung auf meinen selbst gehosteten Weblate Server machst.</translation>
     </message>
     <message id="id-sources">
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Quellen</translation>
     </message>
     <message id="id-donations">
         <source>Donations</source>
-        <translation type="unfinished"></translation>
+        <translation>Spenden</translation>
     </message>
     <message id="id-buy-me-a-beer">
         <source>If you like my work why not buy me a beer?</source>
-        <translation type="unfinished"></translation>
+        <translation>Wenn Du meine Arbeit magst, warum gibst du mir nicht einfach ein Bier aus?</translation>
     </message>
     <message id="id-donate-paypal">
         <source>Donate with PayPal</source>
-        <translation type="unfinished"></translation>
+        <translation>Spenden via PayPal</translation>
     </message>
     <message id="id-donate-liberpay">
         <source>Donate with Liberpay</source>
-        <translation type="unfinished"></translation>
+        <translation>Spenden via Liberpay</translation>
     </message>
     <message id="id-tutorial">
         <source>Tutorial</source>
-        <translation type="unfinished"></translation>
+        <translation>Anleitung</translation>
     </message>
     <message id="id-mesurement-tools">
         <source>Measurement tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Messwerkzeuge</translation>
     </message>
     <message id="id-next">
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Weiter</translation>
     </message>
     <message id="id-reset-mesurement-tools">
         <source>Reset measurement tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Messwerkzeuge zurücksetzen</translation>
     </message>
     <message id="id-previous">
         <source>Previous</source>
-        <translation type="unfinished"></translation>
+        <translation>Zurück</translation>
     </message>
     <message id="id-measure-long-distance">
         <source>Measure long distance</source>
-        <translation type="unfinished"></translation>
+        <translation>Lange Distanzen messen</translation>
     </message>
     <message id="id-mesurement-tools-desc4">
         <source>You can move the measurement tools by dragging them with your finger.</source>
         <oldsource>You can move the measurement tools by draging them with your finger.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Du kannst die Messwerkzeuge mit dem Finger verschieben.</translation>
     </message>
     <message id="id-mesurement-tools-desc2">
         <source>The position (P) on the scale is shown on left side.</source>
-        <translation type="unfinished"></translation>
+        <translation>Die Position (P) auf der Skala wird auf der linken Seite angezeigt.</translation>
     </message>
     <message id="id-mesurement-tools-desc3">
         <source>The second one also shows the length (L) between the two tools.</source>
-        <translation type="unfinished"></translation>
+        <translation>Das Zweite zeigt auch die Länge (L) zwischen den beiden Werkzeugen an.</translation>
     </message>
     <message id="id-reset-measurement-tools-desc1">
         <source>You can reset the measurement tool by double-tapping it.</source>
-        <translation type="unfinished"></translation>
+        <translation>Man kann die Messwerkzeuge zurücksetzen, indem man sie doppelt antippt.</translation>
     </message>
     <message id="id-reset-measurement-tools-desc2">
         <source>In the case of the first, this is reset to zero, in the case of the second, both are reset to their starting values.</source>
-        <translation type="unfinished"></translation>
+        <translation>Man kann die Messwerkzeuge zurücksetzen, indem man sie doppelt antippt.</translation>
     </message>
     <message id="id-long-distance-desc1">
         <source>It is also possible to measure distances longer than the display size.</source>
-        <translation type="unfinished"></translation>
+        <translation>Es ist auch möglich Distanzen zu messen, die länger als der Bildschirm selbst sind.</translation>
     </message>
     <message id="id-long-distance-desc2">
         <source>Move the second tool to the end of the display and mark its position on the measurement object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bewege das zweite Werkzeug zum Ende des Bildschirms und markiere die Position auf dem Messobjekt.</translation>
     </message>
     <message id="id-long-distance-desc3">
         <source>Then scroll the scale until the second tool is on the opposit side of the display.</source>
-        <translation type="unfinished"></translation>
+        <translation>Danach scrolle den Maßstab, bis das zweite Werkzeug auf der gegenüberliegenden Seite des Bildschirms ist.</translation>
     </message>
     <message id="id-long-distance-desc4">
         <source>Move your device until the position of the tool fits the mark on the object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bewege nun das Gerät solange, bis das Werkzeug und die Markierung auf dem Objekt übereinander liegen.</translation>
     </message>
     <message id="id-long-distance-desc5">
         <source>Proceed with the same steps until you reached the end of the object.</source>
         <oldsource>Proceed with the same procedure until you reached the end of the object.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Fahre mit den selben Schritten so lange fort, bis das Ende des Objekts erreicht ist.</translation>
     </message>
     <message id="id-finish">
         <source>Finish</source>
-        <translation type="unfinished"></translation>
+        <translation>Abschließen</translation>
     </message>
     <message id="id-reset">
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Zurücksetzen</translation>
     </message>
     <message id="id-resetting-measurement">
         <source>Resetting measurement</source>
-        <translation type="unfinished"></translation>
+        <translation>Setze Messung zurück</translation>
     </message>
 </context>
 <context>
