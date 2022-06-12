99d33d462b724c10974f099b9b5cc5bc5c08da75
Weblate <noreply@weblate.org>
2022-06-12 10:52:35 +0000

    Translated using Weblate (Hungarian)
    Currently translated at 100.0% (29 of 29 strings)
    
    Co-authored-by: Szabó G <xylevel@yahoo.com>
    Translate-URL: http://weblate.nubecula.org/projects/harbour-zollstock/harbour-zollstock/hu/
    Translation: harbour-zollstock/harbour-zollstock

--- translations/harbour-zollstock-hu.ts
+++ translations/harbour-zollstock-hu.ts
@@ -5,47 +5,47 @@
     <name></name>
     <message id="id-app-name">
         <source>Zollstock</source>
-        <translation type="unfinished"></translation>
+        <translation>Mérőléc</translation>
     </message>
     <message id="id-about">
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Névjegy</translation>
     </message>
     <message id="id-about-page-description">
         <source>You need to measure a length, but you don&apos;t have a ruler with you? Then use this app to get the job done.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hosszúságot mérnél, de nincs nálad mérőszalag? Használd ezt az alkalmazást a feladatra.</translation>
     </message>
     <message id="id-translations">
         <source>Translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Fordítások</translation>
     </message>
     <message id="id-about-page-translation-text">
         <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
-        <translation type="unfinished"></translation>
+        <translation>A te nyelveden nem elérhető? Örömmel vesszük, ha hozzájárulsz a projekthez azzal, hogy lefordítod a saját Weblate szerveremen.</translation>
     </message>
     <message id="id-sources">
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Források</translation>
     </message>
     <message id="id-donations">
         <source>Donations</source>
-        <translation type="unfinished"></translation>
+        <translation>Adományok</translation>
     </message>
     <message id="id-buy-me-a-beer">
         <source>If you like my work why not buy me a beer?</source>
-        <translation type="unfinished"></translation>
+        <translation>Ha tetszik a munkám, miért ne vehetnél nekem egy sört?</translation>
     </message>
     <message id="id-donate-paypal">
         <source>Donate with PayPal</source>
-        <translation type="unfinished"></translation>
+        <translation>Adomány PayPal segítségével</translation>
     </message>
     <message id="id-donate-liberpay">
         <source>Donate with Liberpay</source>
-        <translation type="unfinished"></translation>
+        <translation>Adomány Liberapay segítségével</translation>
     </message>
     <message id="id-tutorial">
         <source>Tutorial</source>
-        <translation type="unfinished"></translation>
+        <translation>Bemutató</translation>
     </message>
     <message id="id-device-info">
         <source>Device info</source>
@@ -70,19 +70,19 @@
     </message>
     <message id="id-mesurement-tools">
         <source>Measurement tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Mérőeszközök</translation>
     </message>
     <message id="id-next">
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Tovább</translation>
     </message>
     <message id="id-reset-mesurement-tools">
         <source>Reset measurement tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Mérőeszközök visszaállítása</translation>
     </message>
     <message id="id-previous">
         <source>Previous</source>
-        <translation type="unfinished"></translation>
+        <translation>Előző</translation>
     </message>
     <message id="id-reset-measurement-tools">
         <source>Reset measurement tools</source>
@@ -90,61 +90,61 @@
     </message>
     <message id="id-measure-long-distance">
         <source>Measure long distance</source>
-        <translation type="unfinished"></translation>
+        <translation>Hosszabb távolság mérése</translation>
     </message>
     <message id="id-mesurement-tools-desc4">
         <source>You can move the measurement tools by dragging them with your finger.</source>
         <oldsource>You can move the measurement tools by draging them with your finger.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>A mérőeszközöket az ujjaddal húzva mozgathatod.</translation>
     </message>
     <message id="id-mesurement-tools-desc2">
         <source>The position (P) on the scale is shown on left side.</source>
-        <translation type="unfinished"></translation>
+        <translation>A skálán a pozíció (P) a bal oldalon látható.</translation>
     </message>
     <message id="id-mesurement-tools-desc3">
         <source>The second one also shows the length (L) between the two tools.</source>
-        <translation type="unfinished"></translation>
+        <translation>A második eszköz mutatja a két eszköz közötti távolságot (L) is.</translation>
     </message>
     <message id="id-reset-measurement-tools-desc1">
         <source>You can reset the measurement tool by double-tapping it.</source>
-        <translation type="unfinished"></translation>
+        <translation>A mérőeszközöket dupla koppintással állíthatod vissza.</translation>
     </message>
     <message id="id-reset-measurement-tools-desc2">
         <source>In the case of the first, this is reset to zero, in the case of the second, both are reset to their starting values.</source>
-        <translation type="unfinished"></translation>
+        <translation>Az első esetében ez nullára, a második esetében mindkettő visszaáll a kiindulási értékre.</translation>
     </message>
     <message id="id-long-distance-desc1">
         <source>It is also possible to measure distances longer than the display size.</source>
-        <translation type="unfinished"></translation>
+        <translation>A képernyő méreténél nagyobb távolság is mérhető.</translation>
     </message>
     <message id="id-long-distance-desc2">
         <source>Move the second tool to the end of the display and mark its position on the measurement object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Mozgasd a második eszközt a képernyő végéig, majd jelöld meg a helyzetét a mérendő objektumon.</translation>
     </message>
     <message id="id-long-distance-desc3">
         <source>Then scroll the scale until the second tool is on the opposit side of the display.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ezután görgesd a skálát, amíg a második eszköz a képernyő ellentétes oldalára kerül.</translation>
     </message>
     <message id="id-long-distance-desc4">
         <source>Move your device until the position of the tool fits the mark on the object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Mozgasd a készüléket, hogy az eszköz pozíciója egyezzen az objektumon lévő jelöléssel.</translation>
     </message>
     <message id="id-long-distance-desc5">
         <source>Proceed with the same steps until you reached the end of the object.</source>
         <oldsource>Proceed with the same procedure until you reached the end of the object.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Ismételd a lépéseket, amíg eléred a tárgy végét.</translation>
     </message>
     <message id="id-finish">
         <source>Finish</source>
-        <translation type="unfinished"></translation>
+        <translation>Befejezés</translation>
     </message>
     <message id="id-reset">
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Visszaállítás</translation>
     </message>
     <message id="id-resetting-measurement">
         <source>Resetting measurement</source>
-        <translation type="unfinished"></translation>
+        <translation>Mérés alaphelyzetbe állítása</translation>
     </message>
 </context>
 </TS>
