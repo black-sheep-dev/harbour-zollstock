9ae7eaf105ef664816399c4408f72d91e5014602
Weblate <noreply@weblate.org>
2022-07-17 05:18:58 +0000

    Added translation using Weblate (Swedish)
    Co-authored-by: Tuomas F Nyqvist <tuomas@lumen.fi>

---
+++ translations/harbour-zollstock-sv.ts
@@ -0,0 +1,150 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name></name>
+    <message id="id-app-name">
+        <source>Zollstock</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-about">
+        <source>About</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-about-page-description">
+        <source>You need to measure a length, but you don&apos;t have a ruler with you? Then use this app to get the job done.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-translations">
+        <source>Translations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-about-page-translation-text">
+        <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-sources">
+        <source>Sources</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-donations">
+        <source>Donations</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-buy-me-a-beer">
+        <source>If you like my work why not buy me a beer?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-donate-paypal">
+        <source>Donate with PayPal</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-donate-liberpay">
+        <source>Donate with Liberpay</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-tutorial">
+        <source>Tutorial</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-device-info">
+        <source>Device info</source>
+        <translation type="vanished">Device info</translation>
+    </message>
+    <message id="id-pixel-density">
+        <source>Pixel/mm</source>
+        <oldsource>Pixel density</oldsource>
+        <translation type="vanished">Pixel/mm</translation>
+    </message>
+    <message id="id-pixel-density-value">
+        <source>%1 Pixel per mm</source>
+        <translation type="vanished">%1 Pixel per mm</translation>
+    </message>
+    <message id="id-section-measure-long-parts">
+        <source>Measure long distance</source>
+        <translation type="vanished">Measure long distance</translation>
+    </message>
+    <message id="id-pixel-denity-desc">
+        <source>The accuracy depends on the pixel density of your device.</source>
+        <translation type="vanished">The accuracy depends on the pixel density of your device.</translation>
+    </message>
+    <message id="id-mesurement-tools">
+        <source>Measurement tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-next">
+        <source>Next</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-reset-mesurement-tools">
+        <source>Reset measurement tools</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-previous">
+        <source>Previous</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-reset-measurement-tools">
+        <source>Reset measurement tools</source>
+        <translation type="vanished">Reset measurement tools</translation>
+    </message>
+    <message id="id-measure-long-distance">
+        <source>Measure long distance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-mesurement-tools-desc4">
+        <source>You can move the measurement tools by dragging them with your finger.</source>
+        <oldsource>You can move the measurement tools by draging them with your finger.</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-mesurement-tools-desc2">
+        <source>The position (P) on the scale is shown on left side.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-mesurement-tools-desc3">
+        <source>The second one also shows the length (L) between the two tools.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-reset-measurement-tools-desc1">
+        <source>You can reset the measurement tool by double-tapping it.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-reset-measurement-tools-desc2">
+        <source>In the case of the first, this is reset to zero, in the case of the second, both are reset to their starting values.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-long-distance-desc1">
+        <source>It is also possible to measure distances longer than the display size.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-long-distance-desc2">
+        <source>Move the second tool to the end of the display and mark its position on the measurement object.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-long-distance-desc3">
+        <source>Then scroll the scale until the second tool is on the opposit side of the display.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-long-distance-desc4">
+        <source>Move your device until the position of the tool fits the mark on the object.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-long-distance-desc5">
+        <source>Proceed with the same steps until you reached the end of the object.</source>
+        <oldsource>Proceed with the same procedure until you reached the end of the object.</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-finish">
+        <source>Finish</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-reset">
+        <source>Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message id="id-resetting-measurement">
+        <source>Resetting measurement</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
