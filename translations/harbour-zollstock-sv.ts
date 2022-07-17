c3fe0f82c79ad96febf3908b3581aeca83753fbc
Weblate <noreply@weblate.org>
2022-07-17 07:55:10 +0000

    Translated using Weblate (Swedish)
    Currently translated at 100.0% (29 of 29 strings)
    
    Co-authored-by: Tuomas F Nyqvist <tuomas@lumen.fi>
    Translate-URL: http://weblate.nubecula.org/projects/harbour-zollstock/harbour-zollstock/sv/
    Translation: harbour-zollstock/harbour-zollstock

--- translations/harbour-zollstock-sv.ts
+++ translations/harbour-zollstock-sv.ts
@@ -5,47 +5,47 @@
     <name></name>
     <message id="id-app-name">
         <source>Zollstock</source>
-        <translation type="unfinished"></translation>
+        <translation>Zollstock</translation>
     </message>
     <message id="id-about">
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>Om Zollstock</translation>
     </message>
     <message id="id-about-page-description">
         <source>You need to measure a length, but you don&apos;t have a ruler with you? Then use this app to get the job done.</source>
-        <translation type="unfinished"></translation>
+        <translation>Du behöver mäta en längd, men du har ingen linjal med dig? Med denna app får du det gjort.</translation>
     </message>
     <message id="id-translations">
         <source>Translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Översättningar</translation>
     </message>
     <message id="id-about-page-translation-text">
         <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Hjälp mig att översätta denna app till flera språk genom att besöka min Weblate server.</translation>
     </message>
     <message id="id-sources">
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Källkod</translation>
     </message>
     <message id="id-donations">
         <source>Donations</source>
-        <translation type="unfinished"></translation>
+        <translation>Donationer</translation>
     </message>
     <message id="id-buy-me-a-beer">
         <source>If you like my work why not buy me a beer?</source>
-        <translation type="unfinished"></translation>
+        <translation>Tycker du om vad jag har gjort så bjud mig en öl?</translation>
     </message>
     <message id="id-donate-paypal">
         <source>Donate with PayPal</source>
-        <translation type="unfinished"></translation>
+        <translation>Donera med PayPal</translation>
     </message>
     <message id="id-donate-liberpay">
         <source>Donate with Liberpay</source>
-        <translation type="unfinished"></translation>
+        <translation>Donera med Liberpay</translation>
     </message>
     <message id="id-tutorial">
         <source>Tutorial</source>
-        <translation type="unfinished"></translation>
+        <translation>Handledning</translation>
     </message>
     <message id="id-device-info">
         <source>Device info</source>
@@ -70,19 +70,19 @@
     </message>
     <message id="id-mesurement-tools">
         <source>Measurement tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Mätverktyg</translation>
     </message>
     <message id="id-next">
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Nästa</translation>
     </message>
     <message id="id-reset-mesurement-tools">
         <source>Reset measurement tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Återställ mätverktyg</translation>
     </message>
     <message id="id-previous">
         <source>Previous</source>
-        <translation type="unfinished"></translation>
+        <translation>Föregående</translation>
     </message>
     <message id="id-reset-measurement-tools">
         <source>Reset measurement tools</source>
@@ -90,61 +90,61 @@
     </message>
     <message id="id-measure-long-distance">
         <source>Measure long distance</source>
-        <translation type="unfinished"></translation>
+        <translation>Mät långa avstånd</translation>
     </message>
     <message id="id-mesurement-tools-desc4">
         <source>You can move the measurement tools by dragging them with your finger.</source>
         <oldsource>You can move the measurement tools by draging them with your finger.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Du kan flytta mätverktygen genom att dra dem med fingret.</translation>
     </message>
     <message id="id-mesurement-tools-desc2">
         <source>The position (P) on the scale is shown on left side.</source>
-        <translation type="unfinished"></translation>
+        <translation>Positionen (P) på skalan visas på vänster sida.</translation>
     </message>
     <message id="id-mesurement-tools-desc3">
         <source>The second one also shows the length (L) between the two tools.</source>
-        <translation type="unfinished"></translation>
+        <translation>Den andra visar också längden (L) mellan de två verktygen.</translation>
     </message>
     <message id="id-reset-measurement-tools-desc1">
         <source>You can reset the measurement tool by double-tapping it.</source>
-        <translation type="unfinished"></translation>
+        <translation>Du kan återställa mätverktyget genom att dubbeltrycka på det.</translation>
     </message>
     <message id="id-reset-measurement-tools-desc2">
         <source>In the case of the first, this is reset to zero, in the case of the second, both are reset to their starting values.</source>
-        <translation type="unfinished"></translation>
+        <translation>I fallet med det första nollställs detta, i fallet med det andra återställs båda till sina startvärden.</translation>
     </message>
     <message id="id-long-distance-desc1">
         <source>It is also possible to measure distances longer than the display size.</source>
-        <translation type="unfinished"></translation>
+        <translation>Det är också möjligt att mäta avstånd längre än displaystorleken.</translation>
     </message>
     <message id="id-long-distance-desc2">
         <source>Move the second tool to the end of the display and mark its position on the measurement object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Flytta det andra verktyget till slutet av displayen och markera dess position på mätobjektet.</translation>
     </message>
     <message id="id-long-distance-desc3">
         <source>Then scroll the scale until the second tool is on the opposit side of the display.</source>
-        <translation type="unfinished"></translation>
+        <translation>Dra sedan skalan tills det andra verktyget är på motsatt sida av displayen.</translation>
     </message>
     <message id="id-long-distance-desc4">
         <source>Move your device until the position of the tool fits the mark on the object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Flytta enheten tills verktygets position passar märket på mätobjektet.</translation>
     </message>
     <message id="id-long-distance-desc5">
         <source>Proceed with the same steps until you reached the end of the object.</source>
         <oldsource>Proceed with the same procedure until you reached the end of the object.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Fortsätt med samma steg tills du når slutet av objektet.</translation>
     </message>
     <message id="id-finish">
         <source>Finish</source>
-        <translation type="unfinished"></translation>
+        <translation>Avsluta</translation>
     </message>
     <message id="id-reset">
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Återställ</translation>
     </message>
     <message id="id-resetting-measurement">
         <source>Resetting measurement</source>
-        <translation type="unfinished"></translation>
+        <translation>Återställer mätningen</translation>
     </message>
 </context>
 </TS>
