e2ab4c72798920b97baa2bde05186bb23ea9059e
Weblate <noreply@weblate.org>
2022-08-18 22:48:25 +0000

    Translated using Weblate (Russian)
    Currently translated at 97.0% (33 of 34 strings)
    
    Co-authored-by: Dmitriy <dimoon91@yandex.ru>
    Translate-URL: http://weblate.nubecula.org/projects/harbour-zollstock/harbour-zollstock/ru/
    Translation: harbour-zollstock/harbour-zollstock

--- translations/harbour-zollstock-ru.ts
+++ translations/harbour-zollstock-ru.ts
@@ -9,43 +9,43 @@
     </message>
     <message id="id-about">
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>О программе</translation>
     </message>
     <message id="id-about-page-description">
         <source>You need to measure a length, but you don&apos;t have a ruler with you? Then use this app to get the job done.</source>
-        <translation type="unfinished"></translation>
+        <translation>Тебе нужно измерить длину, но под рукой нет линейки? Это приложение поможет тебе.</translation>
     </message>
     <message id="id-translations">
         <source>Translations</source>
-        <translation type="unfinished"></translation>
+        <translation>Переводы</translation>
     </message>
     <message id="id-about-page-translation-text">
         <source>Your language is not available? You are welcome to support this project by translating it on my self hosted Weblate server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ваш язык недоступен? Поддержите проект, переведя на Weblate.</translation>
     </message>
     <message id="id-sources">
         <source>Sources</source>
-        <translation type="unfinished"></translation>
+        <translation>Исходники</translation>
     </message>
     <message id="id-donations">
         <source>Donations</source>
-        <translation type="unfinished"></translation>
+        <translation>На пиво</translation>
     </message>
     <message id="id-buy-me-a-beer">
         <source>If you like my work why not buy me a beer?</source>
-        <translation type="unfinished"></translation>
+        <translation>Если тебе понравилась моя работа, то можешь угостить меня пивом.</translation>
     </message>
     <message id="id-donate-paypal">
         <source>Donate with PayPal</source>
-        <translation type="unfinished"></translation>
+        <translation>Через PayPal</translation>
     </message>
     <message id="id-donate-liberpay">
         <source>Donate with Liberpay</source>
-        <translation type="unfinished"></translation>
+        <translation>Через Liberpay</translation>
     </message>
     <message id="id-tutorial">
         <source>Tutorial</source>
-        <translation type="unfinished"></translation>
+        <translation>Инструкция</translation>
     </message>
     <message id="id-device-info">
         <source>Device info</source>
@@ -70,19 +70,19 @@
     </message>
     <message id="id-mesurement-tools">
         <source>Measurement tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Границы замера</translation>
     </message>
     <message id="id-next">
         <source>Next</source>
-        <translation type="unfinished"></translation>
+        <translation>Далее</translation>
     </message>
     <message id="id-reset-mesurement-tools">
         <source>Reset measurement tools</source>
-        <translation type="unfinished"></translation>
+        <translation>Сброс измерений</translation>
     </message>
     <message id="id-previous">
         <source>Previous</source>
-        <translation type="unfinished"></translation>
+        <translation>Назад</translation>
     </message>
     <message id="id-reset-measurement-tools">
         <source>Reset measurement tools</source>
@@ -90,81 +90,81 @@
     </message>
     <message id="id-measure-long-distance">
         <source>Measure long distance</source>
-        <translation type="unfinished"></translation>
+        <translation>Измерь большую длину</translation>
     </message>
     <message id="id-mesurement-tools-desc4">
         <source>You can move the measurement tools by dragging them with your finger.</source>
         <oldsource>You can move the measurement tools by draging them with your finger.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Вы можете перемещать границы измерения, перетаскивая их пальцем.</translation>
     </message>
     <message id="id-mesurement-tools-desc2">
         <source>The position (P) on the scale is shown on left side.</source>
-        <translation type="unfinished"></translation>
+        <translation>Слева на шкале находится начальная граница замера.</translation>
     </message>
     <message id="id-mesurement-tools-desc3">
         <source>The second one also shows the length (L) between the two tools.</source>
-        <translation type="unfinished"></translation>
+        <translation>Вторая линия отображает длину (L) между двумя границами.</translation>
     </message>
     <message id="id-reset-measurement-tools-desc1">
         <source>You can reset the measurement tool by double-tapping it.</source>
-        <translation type="unfinished"></translation>
+        <translation>Можно сбросить положение границы двойным тапом.</translation>
     </message>
     <message id="id-reset-measurement-tools-desc2">
         <source>In the case of the first, this is reset to zero, in the case of the second, both are reset to their starting values.</source>
-        <translation type="unfinished"></translation>
+        <translation>В случае первой границы значение сбрасывается на ноль, в случае второй - обе границы сбрасываются до исходных значений.</translation>
     </message>
     <message id="id-long-distance-desc1">
         <source>It is also possible to measure distances longer than the display size.</source>
-        <translation type="unfinished"></translation>
+        <translation>Можно измерять длину, превышающую размер экрана.</translation>
     </message>
     <message id="id-long-distance-desc2">
         <source>Move the second tool to the end of the display and mark its position on the measurement object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Переместите вторую границу в конец экрана и отметьте её положение на объекте измерения.</translation>
     </message>
     <message id="id-long-distance-desc3">
         <source>Then scroll the scale until the second tool is on the opposit side of the display.</source>
-        <translation type="unfinished"></translation>
+        <translation>Затем прокручивайте шкалу до тех пор, пока граница не окажется в начале экрана.</translation>
     </message>
     <message id="id-long-distance-desc4">
         <source>Move your device until the position of the tool fits the mark on the object.</source>
-        <translation type="unfinished"></translation>
+        <translation>Перемещайте смартфон до тех пор, пока положение границы не будет соответствовать отметке на объекте.</translation>
     </message>
     <message id="id-long-distance-desc5">
         <source>Proceed with the same steps until you reached the end of the object.</source>
         <oldsource>Proceed with the same procedure until you reached the end of the object.</oldsource>
-        <translation type="unfinished"></translation>
+        <translation>Продолжайте выполнять эти действия, пока не дойдете до конца измеряемого объекта.</translation>
     </message>
     <message id="id-finish">
         <source>Finish</source>
-        <translation type="unfinished"></translation>
+        <translation>Готово</translation>
     </message>
     <message id="id-reset">
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation>Сброс</translation>
     </message>
     <message id="id-resetting-measurement">
         <source>Resetting measurement</source>
-        <translation type="unfinished"></translation>
+        <translation>Сброс измерения</translation>
     </message>
     <message id="id-settings">
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Параметры</translation>
     </message>
     <message id="id-calibration">
         <source>Calibration</source>
-        <translation type="unfinished"></translation>
+        <translation>Калибровка</translation>
     </message>
     <message id="id-calibration-desc-1">
         <source>This app uses the pixel density per millimeter reported by the device as the calibration for the scale.</source>
-        <translation type="unfinished"></translation>
+        <translation>Это приложение использует плотность пикселей на миллиметр, сообщаемую устройством, для калибровки шкалы.</translation>
     </message>
     <message id="id-calibration-desc-2">
         <source>You can change this with a scaling factor in the input field below.</source>
-        <translation type="unfinished"></translation>
+        <translation>Вы можете изменить это с помощью коэффициента масштабирования в поле ввода ниже.</translation>
     </message>
     <message id="id-scaling-factor">
         <source>Scaling factor</source>
-        <translation type="unfinished"></translation>
+        <translation>Коэффициент масштабирования</translation>
     </message>
 </context>
 </TS>
